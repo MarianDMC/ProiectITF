de28482c551c0f8ae51e832c143a91316535b602
MarianDMC <123266007+MarianDMC@users.noreply.github.com>
2024-06-16 20:54:09 +0300

    Delete PROIECTItf/beersite directory

--- PROIECTItf/beersite/shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path('<int:beer_id>', views.beer_details, name='beer_details'),
-    path('beers', views.all_beers, name='all_beers')
-]
